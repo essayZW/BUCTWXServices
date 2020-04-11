206efffa2f705f9aed10f0a550642a710e8e7964
xmBryant24 <wxhxm_24@163.com>
2020-04-11 11:19:37 +0800

    添加获得用户成绩API

--- app/view/jw.py
+++ app/view/jw.py
@@ -1,9 +1,7 @@
 # -*- coding: utf8 -*-
-from flask import Blueprint, request
+from flask import Blueprint,render_template,Flask,request,json
 # 引入教务爬虫
 from ..moudle.jwrobot import Robot
-
-import json
 # 创建教务蓝图
 jwBlueprint = Blueprint('jw', __name__)
 
@@ -14,13 +12,17 @@ def getStuInfo():
         'info' : '',
         'data' : None
     }
-    if not request.form.get('username') or not request.form.get('password') or not request.form.get('vpnusername') or not request.form.get('vpnpassword'):
+    userName = request.form.get('username')
+    passWord = request.form.get('password')
+    vpnUserName = request.form.get('vpnusername')
+    vpnPassWord = request.form.get('vpnpassword')
+    if not userName or not passWord or not vpnUserName or not vpnPassWord:
         res['info'] = '缺少参数'
         return json.dumps(res)
-    robot = Robot('https://jwglxt.w.buct.edu.cn', request.form.get('username'), request.form.get('password'))
-    vpnlogon = robot.vpnLogin(request.form.get('vpnusername'), request.form.get('vpnpassword'))
+    robot = Robot('https://jwglxt.w.buct.edu.cn', userName, passWord)
+    vpnlogon = robot.vpnLogin(vpnUserName, vpnPassWord)
     if not vpnlogon:
-        res['info'] = 'VPN用户民或者密码错误'
+        res['info'] = 'VPN用户名或者密码错误'
         return json.dumps(res)
     robot.login()
     if not robot.isLogin():
@@ -32,4 +34,41 @@ def getStuInfo():
     res['info'] = 'success'
     return json.dumps(res)
 
+
+@jwBlueprint.route('/getAllGrade',methods = ['POST'])
+def getAllGrade():
+    res = {
+        'status' : False,
+        'info' : '',
+        'data' : None
+    }
     
+    userName = request.form.get('username')
+    passWord = request.form.get('password')
+    vpnUserName = request.form.get('vpnusername')
+    vpnPassWord = request.form.get('vpnpassword')
+    if not userName or not passWord or not vpnUserName or not vpnPassWord:
+        res['info'] = '缺少参数'
+        return json.dumps(res)
+    robot = Robot('https://jwglxt.w.buct.edu.cn', userName, passWord)
+    vpnlogon = robot.vpnLogin(vpnUserName, vpnPassWord)
+    if not vpnlogon:
+        res['info'] = 'VPN用户名或者密码错误'
+        return json.dumps(res)
+    robot.login()
+    if not robot.isLogin():
+        res['info'] = '用户名或者密码错误'
+        return json.dumps(res)
+    
+    xnm = request.form.get('xnm')
+    xqm = request.form.get('xqm')
+    xqm = [3, 12, 16][int(xqm) - 1]
+
+    allGrade = robot.getGrade(xnm,xqm)
+    res['status'] = True
+    res['data'] = allGrade
+    res['info'] = 'success'
+    return json.dumps(res)
+
+@jwBlueprint.route('/getDetailScore',methods = ['POST'])
+def getDetailScore():
