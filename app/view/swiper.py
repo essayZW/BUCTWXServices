3b5e5df34da799bc2336332c194e1256d4eeb632
essay <dazw2000@163.com>
2020-05-13 22:07:38 +0800

    修改通知内容

--- app/view/swiper.py
+++ app/view/swiper.py
@@ -2,9 +2,29 @@
 from flask import Blueprint
 import json
 swiperBlueprint = Blueprint('swiper', __name__)
+# 由于事件关系，暂时不链接数据库，内容写在文件里面
 
 @swiperBlueprint.route('/getAll', methods=['POST'])
 def getAllSwiperData():
+    notice = '''# 新版功能
+
+## 教务
+
+* 成绩查询
+* 成绩详情查询
+* GPA查询
+* 考试信息查询
+
+## 校园生活
+
+* 日历功能
+* 待办事件
+* 自动整合考试信息到日历待办
+* 待办与考试信息提醒
+
+## 其他
+
+* 自定义主题颜色切换'''
     return json.dumps({
         'data' : [
             {
@@ -27,7 +47,7 @@ def getAllSwiperData():
                 'type' : 'alert',
                 'dataset' : {
                     'src' : '',
-                    'alertcontent' : '暂无'
+                    'alertcontent' : notice
                 },
                 'image' : 'https://s1.ax1x.com/2020/05/13/YamWdg.png'
             }
